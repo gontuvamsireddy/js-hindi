6a583a921bd27f40e89cbbcf42be574403b353f9
gontuvamsireddy <133561877+gontuvamsireddy@users.noreply.github.com>
2023-10-14 16:35:12 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version.
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log(3
+    +3) //code readability should be high
+
+console.log("vamsi")
+ 
+let name ="krishna"  //string
+let age =22 //number
+let isLoggedIn = false
+let state;
+
+//number=> 2 to power 53
+//bigInt
+//String=>"",''
+//boolean => true/false
+//null=> standalone value
+//undefined =>
+//symbol => uniqu
+console.log(typeof("vamsi"))
+//object
+console.log(typeof(null))       //object
+console.log(typeof undefined)  //undefined
+
---
+++ basics/variable.js
@@ -0,0 +1,33 @@
+const accountId = 144553
+let accountEmail ="vamsi36560@gmail.com"
+var accountPassword ="12345"
+accountCity ="jaipur"
+let accountState;
+
+//accountId = 2 //not allowed
+
+
+
+accountEmail ="vamsi@"
+accountPassword ="2656"
+accountCity ="Bengaluru"
+
+console.log(accountId)
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+
+
+Var
+variables are globally scoped or function scoped. They can be reassigned and redeclared within their scope.
+
+Let
+variables are block scoped. They can be reassigned but not redeclared within their scope.
+
+Const
+variables are block scoped and cannot be reassigned or redeclared. They must be initialized when they are declared.
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
