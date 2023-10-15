5d33b04751f548e16e914ac7c07e462e6948ab38
gontuvamsireddy <133561877+gontuvamsireddy@users.noreply.github.com>
2023-10-15 16:46:04 +0000

    datatypes-summary

--- basics/datatypes-summary.js
+++ basics/datatypes-summary.js
@@ -5,33 +5,56 @@
 //7 type : String,Number,Boolean,null,undefined,Symbol,BigInt
 
 
-const score = 100
-const scoreValue =100.3
+// const score = 100
+// const scoreValue =100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id ==anotherId)
+// console.log(id ==anotherId)
 
-const bigNumber = 35454878784515n
+// const bigNumber = 35454878784515n
 
 
 
-//Reference (Non primitive)
+// //Reference (Non primitive)
 
-//Array,Objects,Functions
+// //Array,Objects,Functions
 
-const heros =["shaktiman","naagraj","dooga"]
-let myObj = {
-    name: "vamsi",
-    age:22,
+// const heros =["shaktiman","naagraj","dooga"]
+// let myObj = {
+//     name: "vamsi",
+//     age:22,
+// }
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof anotherId);    
+
+// ++++++++++++++++++++++++++++++++++++
+//Stack(primitive)
+
+//Heap (Non-primitive)
+
+let myName ="vamsi"
+let names = myName
+
+names = "chai our code"
+console.log(names);
+console.log(myName)
+
+
+let user ={
+    email:"vamsi@gmail.com",
+    upi:"user1"
 }
-const myFunction = function(){
-    console.log("Hello world");
-}
 
-console.log(typeof anotherId);    
+let usertwo = user
+usertwo.email ="vamsi@gmail.com"
+console.log(user.email)
+console.log(usertwo.email)
