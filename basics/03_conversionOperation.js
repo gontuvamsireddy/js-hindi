7f857436ff0603f38616a766c512891703d69e86
gontuvamsireddy <133561877+gontuvamsireddy@users.noreply.github.com>
2023-10-15 03:37:50 +0000

    conversionOperation

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score =null
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
 
 
 // "33" => 33
@@ -16,7 +16,7 @@ let isLoggedIn  = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1=> true; 0=> fasle
 //"" => false
@@ -24,5 +24,43 @@ console.log(booleanIsLoggedIn)
 
 let someNumber =33
 let stringNumber = String(someNumber)
-console.log(typeof(stringNumber))
-console.log(stringNumber)
+// console.log(typeof(stringNumber))
+// console.log(stringNumber)
+
+
+//*******************Operations******************* */
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 ="hello"
+let str2 = " vamsi"
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1+"2")
+// console.log("1" + 2 +2)
+// console.log(1 + 2 +"2")
+
+// console.log(3+4*5%3)
+
+// console.log(+true)
+// console.log(+"");
+
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
