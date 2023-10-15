c5836884a4f8fc2efdfe362e857889e3cd2350ec
gontuvamsireddy <133561877+gontuvamsireddy@users.noreply.github.com>
2023-10-15 17:33:22 +0000

    Strings

---
+++ basics/05_Strings.js
@@ -0,0 +1,37 @@
+// Strings
+// const name ="vamsi"
+// const repoCount =50
+
+//console.log(name + repoCount + " value")
+
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)
+
+ const gameName = new String('vamsi-krishna')
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+//console.log(gameName.indexOf('m'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,8)
+// console.log(anotherString);
+
+const newStringOne = "      vamsi  ";
+console.log(newStringOne);
+console.log(newStringOne.trim())
+
+const url = "https://hithesh.com/hithesh20"
+console.log(url.replaceAll('hithesh','vamsi'))
+
+
+console.log(url.includes('hithesh'))
+
+console.log(url.split('/'))
+
+
