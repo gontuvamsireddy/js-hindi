cfde4ec78ae6b728d0b72f86fee2b9b49a802691
gontuvamsireddy <133561877+gontuvamsireddy@users.noreply.github.com>
2023-10-20 06:02:27 +0000

    scope

--- basics/11_scopes.js
+++ basics/11_scopes.js
@@ -1,13 +1,49 @@
 
 //var c  =300
-let a =300
+// let a =300
 
-if(true){
-    let a =10
-    const b = 20
-    console.log("INNER: ",a);
+// if(true){
+//     let a =10
+//     const b = 20
+//     console.log("INNER: ",a);
+// }
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+function one(){
+    const username ="vamsi"
+    function two(){
+        const website ="youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
 }
 
-console.log(a)
-console.log(b)
-console.log(c)
+//one()
+
+if(true){
+    const username = "vamsi"
+    if(username === "vamsi"){
+        const website = "youtube"
+        //console.log(username+" "+ website)
+    } 
+}
+
+// ++++++++++++++++++++++++++++++++interesting+++++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(value){
+    return num +1
+}
+
+addTwo(5)
+const addTwo = function(num){
+    return num+2
+}
+
