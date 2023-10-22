174092f5a49290b7552ef9907263b64acc00423f
gontuvamsireddy <133561877+gontuvamsireddy@users.noreply.github.com>
2023-10-22 16:37:51 +0000

    Iterations

---
+++ basics/16_Iterations.js
@@ -0,0 +1,21 @@
+//  for
+
+for(let index =0;index<10;index++){
+    const element = index;
+    console.log(element);
+
+}
+
+//break and continue
+//while loop
+//while(condition_check){
+
+//}
+
+//do while
+
+let score = 1
+do{
+    console.log(`score is ${score}`);
+    score++;
+}while(score <=10);
