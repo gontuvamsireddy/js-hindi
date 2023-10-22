7ee8c1ecd2e0104410e8788a68f0028ce388485a
gontuvamsireddy <133561877+gontuvamsireddy@users.noreply.github.com>
2023-10-22 18:15:28 +0000

    mapfilterreduce

---
+++ basics/19_Mapfilterreduce.js
@@ -0,0 +1,28 @@
+//const coding =["js","rubby","java","python","cpp"]
+
+
+// const values = coding.forEach((item) =>{
+//     //console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums  = myNums.filter((num)=> num>4)
+// console.log(newNums);
+
+const newNums = []
+// mynums.forEach((num) =>{
+
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
+
+//Filter
+
+cons userBooks = books.filter((bk) => bk.genre === 'History')
