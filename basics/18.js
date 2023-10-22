260af7170e76883ab68c8fdf984e525579b8f946
gontuvamsireddy <133561877+gontuvamsireddy@users.noreply.github.com>
2023-10-22 17:15:45 +0000

    18

---
+++ basics/18.js
@@ -0,0 +1,14 @@
+const coding = ["js","ruby","java","python","cpp"]
+
+coding.forEach(function (val) {
+    console.log(val);
+})
+
+coding.forEach((item) => {
+    console.log(item)
+})
+
+coding.forEach((item,index,arr)=>{
+    console.log(item,index,arr)
+})
+
