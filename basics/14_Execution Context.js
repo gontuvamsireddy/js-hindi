f313398f1a4144e6ebdbb8c971eca32afe52a196
gontuvamsireddy <133561877+gontuvamsireddy@users.noreply.github.com>
2023-10-22 15:33:13 +0000

    Execution Context

---
+++ basics/14_Execution Context.js
@@ -0,0 +1,27 @@
+//Javascript Execution Context
+
+//{}
+//Steps of Execution of programme
+//1.Global Execution Context--->this
+//2.Memory creation phase
+//3.Function Execution Context
+//4.Evaluation Execution Context
+
+
+//Two Phases
+//1.{} --> Memory creation phase
+
+//2. Execution Phase
+
+
+//new Variable environment  +  thread
+function one(){
+    console.log("one")
+}
+function two(){
+    console.log("two")
+}
+
+
+one()
+two()
